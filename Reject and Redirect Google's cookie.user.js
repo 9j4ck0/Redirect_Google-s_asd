8e4b7cbc41638a83b5b529fe303c1305860f7cc3
9j4ck0 <9j4ck0@users.noreply.github.com>
2021-11-11 14:58:27 +0100

    Update Reject and Redirect Google's cookie.user.js
    redirects the search made on Google

--- Reject and Redirect Google's cookie.user.js
+++ Reject and Redirect Google's cookie.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Reject and Redirect Google's cookie
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       9j3tr0
+// @version      0.1.1
+// @description  Reject and Redirect Google's cookie
+// @author       9j3thr0
 // @match        https://www.google.com/*
 // @match        https://www.google.it/*
 // @match        https://consent.google.it/*
@@ -23,29 +23,30 @@
     if(new RegExp(/\google.*/ig).exec(window.location.href) != null) {
         for(var k = 0; k < x.length; k++) {
             if(x[k].innerHTML.toLowerCase().includes(personalizza.toLowerCase())) {
-                console.log(document.getElementsByTagName("button")[k].getAttribute('id'));
+                // console.log(document.getElementsByTagName("button")[k].getAttribute('id'));
                 document.getElementsByTagName("button")[k].click();
             }
         }
     }
     for(var i = 0; i < x.length; i++) {
         if(x[i].innerHTML.toLowerCase().includes(nonAttiva.toLowerCase()) && count < 3) {
-            console.log(document.getElementsByTagName('button')[i].getAttribute('class'));
+            // console.log(document.getElementsByTagName('button')[i].getAttribute('class'));
             x[i].scrollIntoView(true);
             document.getElementsByTagName('button')[i].click();
             count++;
         }
     }
     setTimeout(function(){
-        console.log('after');
+        var page = window.location.href.split('d?continue=')[1];
         for(var j = 0; j < x.length; j++) {
             if(x[j].innerHTML.toLowerCase().includes(continua.toLowerCase())) {
-                console.log('j = ' + j);
+                // console.log('j = ' + j);
                 x[j].scrollIntoView(true);
                 document.getElementsByTagName('button')[j].click();
+                setTimeout(function(){
+                    window.location.replace(decodeURIComponent(page.split('UTF-8')[0] + 'UTF-8'));
+                },1500);
             }
         }
     },500);
 })();
-
-//http://jsfiddle.net/xvHYw/2/
